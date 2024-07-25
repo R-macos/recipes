91fbecc22db69d7c401f7376d30e0ff4428041af
Simon Urbanek <simon.urbanek@R-project.org>
2024-07-26 09:35:04 +1200

    add --cran option to the big-sur script

--- scripts/big-sur-build.sh
+++ scripts/big-sur-build.sh
@@ -10,16 +10,72 @@ fi
 while (( "$#" )); do
     if [ "x$1" = x--tools ]; then RUN_TOOLS=1; fi
     if [ "x$1" = x--all ]; then RUN_ALL=1; fi
+    if [ "x$1" = x--cran ]; then AS_CRAN=1; fi
     if [ "x$1" = x-h -o "x$1" = x--help ]; then
 	echo ''
-	echo " Usage: $0 [-h|--help] [--tools | --all]"
+	echo " Usage: $0 [-h|--help] [--cran] [--tools | --all]"
 	echo ''
 	echo " Default is --base (r-base-dev), tools include emacs and subversion."
+	echo ''
+	echo ' --cran enforces existence of all tools and locations, otherwise'
+	echo '        they are optional'
 	echo ''
 	exit 0
     fi
     shift
 done
+
+if [ -n "$AS_CRAN" ]; then
+    ENVOK=yes
+    echo Checking build environment ...
+    if [ ! -e /Volumes/Temp ]; then
+	echo "ERROR: /Volumes/Temp does not exist!" >&2
+	ENVOK=no
+    else
+	if [ ! -e /Volumes/Temp/tmp ]; then
+	    mkdir -p /Volumes/Temp/tmp
+	fi
+	echo " - /Volumes/Temp/tmp OK"
+    fi
+    if [ ! -e /Library/Developer/CommandLineTools/SDKs/MacOSX11.sdk ]; then
+	echo "ERROR: macOS 11 SDK is missing" >&2
+	ENVOK=no
+    else
+	echo " - SDK OK"
+    fi
+    if [ ! -e /Applications/CMake.app/Contents/bin/cmake ]; then
+	echo "ERROR: CMake is missing" >&2
+	ENVOK=no
+    else
+	echo " - CMake OK"
+    fi
+    if ! command -v meson > /dev/null; then
+	PYLIB=$(ls -d ~/Library/Python/3.*/bin | tail -n1)
+	if [ -n "$PYLIB" ]; then
+	    export PATH=$PATH:$PYLIB
+	    echo " - Adding Python 3 user library to PATH"
+	fi
+    fi
+    if ! command -v meson > /dev/null; then
+	echo " - Installing meson"
+	pip3 install --user meson
+    fi
+    if ! command -v ninja > /dev/null; then
+	echo " - Installing ninja"
+	pip3 install --user ninja
+    fi
+    command -v meson
+    command -v ninja
+    if [ ! -e ~/.gnu-mirror ]; then
+	## GNU server is terribly slow so set a more sane mirror
+	echo https://mirror.endianness.com/gnu/ > ~/.gnu-mirror
+    fi
+    export PREFIX="opt/R/`uname -m`"
+    echo Setup target /$PREFIX
+    if [ ! -e "/$PREFIX" ]; then
+	mkdir -p "/$PREFIX"
+    fi
+fi
 
 if [ -e /Volumes/Temp/tmp ]; then
     export TMPDIR=/Volumes/Temp/tmp
@@ -64,27 +120,48 @@ fi
 
 ## freetype and harfbuzz have a circular dependency
 ## and need to be bootstrapped in the order FT -> HB -> FT
+echo ''
+echo " ----- Bootstrapping freetype"
+echo ''
 ./build.sh -f -p freetype
 ./build.sh -f -p harfbuzz
 rm -rf build/freetype-2.*
+echo ''
+echo " ----- Re-building freetype with Harfbuzz"
+echo ''
+./build.sh -f -p freetype
 
 ## required
+echo ''
+echo " ----- Building r-base-dev"
+echo ''
 ./build.sh -f -p r-base-dev
 
 ## NOTE: CRAN R also uses: readline5 pango
+if [ -n "$AS_CRAN" ]; then
+    ./build.sh -f -p readline5
+    ## pango has to be built last due to glib causing issues
+fi
 
 ## useful
 if [ -n "$RUN_TOOLS" ]; then
+    echo ''
+    echo " ----- Building tools"
+    echo ''
     ./build.sh -f -p subversion emacs
 fi
 
 ## all others
 if [ -n "$RUN_ALL" ]; then
+    echo ''
+    echo " ----- Building all"
+    echo ''
     ./build.sh -f -p all
 fi
 
-## NOTE: if you want to disable fail-fast, use
-## ./build.sh -f -p -- -k all
+echo ''
+echo ' NOTE: if you want to disable fail-fast, use'
+echo ' ./build.sh -f -p -- -k all'
 
 echo ''
 echo "=== DONE"
